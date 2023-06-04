a8da1dcee6c85baf6b133d8b56f9e2103af4dc0a
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-06-04 18:07:27 +0200

    created update and delete post routes

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -9,12 +9,50 @@ const getPost = async (req, res) => {
 		}
 		res.json(post)
 	} catch (error) {
-		res
-			.status(500)
-			.json({
-				message: "Error occurred while fetching posts: " + error.message,
-			})
+		res.status(500).json({
+			message: "Error occurred while fetching posts: " + error.message,
+		})
+	}
+}
+const deletePost = async (req, res) => {
+	const { id } = req.body
+
+	if (!id) {
+		return res.status(400).json({ message: "Invalid id." })
+	}
+	try {
+		const post = await Post.findById(id)
+
+		if (!post) {
+			return res.status(404).json({ message: "Post not found." })
+		}
+
+		await post.remove()
+
+		return res.status(200).json({ message: "Post deleted successfully." })
+	} catch (error) {
+		return res.status(500).json({ message: "Server error." })
+	}
+}
+const updatePost = async (req, res) => {
+	const { id, content } = req.body
+
+	try {
+		const post = await Post.findById(id)
+
+		if (!post) {
+			return res.status(404).json({ message: "Post not found." })
+		}
+
+		post.content = content
+		const updatedPost = await post.save()
+
+		return res
+			.status(200)
+			.json({ message: "Post updated successfully.", post: updatedPost })
+	} catch (error) {
+		return res.status(500).json({ message: "Server error." })
 	}
 }
 
-module.exports = { getPost }
+module.exports = { getPost, deletePost, updatePost }
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
-const { getPost } = require("../controllers/postController")
+const {
+	getPost,
+	deletePost,
+	updatePost,
+} = require("../controllers/postController")
 
 router.get("/article", getPost)
+router.delete("/article", deletePost)
+router.patch("/article", updatePost)
 
 module.exports = router
