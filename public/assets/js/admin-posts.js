8e0c23917755ea4ed49b343a1e6e8b2b0b91a932
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-06-04 20:28:16 +0200

    finish admi-post

--- public/assets/js/admin-posts.js
+++ public/assets/js/admin-posts.js
@@ -1,17 +1,47 @@
 const adminNick = document.querySelector(".nav__nick")
 const storedUser = localStorage.getItem("6546613999528")
-const editBtn = document.getElementById("edit")
-const deleteBtn = document.getElementById("delete")
-const saveBtn = document.getElementById("save")
-const confirmDeleteBtn = document.getElementById("confirm-delete")
 
 if (storedUser) {
 	const nick = JSON.parse(storedUser)
 	adminNick.textContent = nick.username
 }
 
+async function deletePost(id) {
+	try {
+		const response = await fetch("/article", {
+			method: "DELETE",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ id }),
+		})
 
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
 
+		const data = await response.json()
+		displayPosts()
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+async function editPost(id, content) {
+	try {
+		const response = await fetch("/article", {
+			method: "PATCH",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ id, content }),
+		})
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
+
+		const data = await response.json()
+		displayPosts()
+	} catch (error) {
+		console.log(error)
+	}
+}
 
 async function displayPosts() {
 	try {
@@ -24,37 +54,71 @@ async function displayPosts() {
 			throw new Error(`HTTP error! status: ${response.status}`)
 		}
 
-		const data = await response.json()
 		const container = document.querySelector(".posts")
-		console.log(data)
-		for (const el of data) {
+		container.textContent = ""
+
+		const data = await response.json()
+		for (const [index, el] of data.entries()) {
 			const html = `<div class="posts__post">
 		<div class="posts__post-header">
 			<p class="post-owner">${el.username}</p>
 			<div class="posts__post-header--action">
-				<button id="save" class="btn" type="submit">Save</button>
-				<button id="edit" class="btn" type="submit">Edit</button>
-				<button id="delete" class="btn">Delete</button>
+				<button class="btn save" onclick="updatePost(${index},'${el._id}')">Save</button>
+				<button class="btn edit" onclick="startEdit(${index})">Edit</button>
+				<button class="btn delete" onclick="startDelete(${index})">Delete</button>
 			</div>
 		</div>
 		<textarea
 			name="post-content"
-			data-id="${el.id}"
+			data-id="${el._id}"
 			class="posts__post-content"
 			disabled
 		>${el.content}</textarea>
 		<div class="post-confirmation">
-			<button id="confirm-delete" class="btn" type="submit">
+			<button class="btn confirm-delete" onclick="deletePost('${el._id}')">
 				Confirm delete
 			</button>
-			<button class="btn cancel">Cancel</button>
+			<button class="btn cancel" onclick="abordDelete(${index})">Cancel</button>
 		</div>
 	</div>`
+
 			container.insertAdjacentHTML("beforeend", html)
 		}
 	} catch (error) {
 		console.log(error)
 	}
+}
+
+const startDelete = (index) => {
+	const delBgc = document.querySelectorAll(".post-confirmation")
+	delBgc[index].style.display = "flex"
+}
+const abordDelete = (index) => {
+	const delBgc = document.querySelectorAll(".post-confirmation")
+	delBgc[index].style.display = ""
+}
+
+const startEdit = (index) => {
+	const textArea = document.querySelectorAll(".posts__post-content")
+	const saveBtn = document.querySelectorAll(".save")
+	const editBtn = document.querySelectorAll(".edit")
+	textArea[index].removeAttribute("disabled")
+	textArea[index].classList.add("textarea-on")
+	saveBtn[index].classList.add("show-btn")
+	editBtn[index].classList.add("hide")
+}
+const updatePost = (index, id) => {
+	const textArea = document.querySelectorAll(".posts__post-content")
+	const saveBtn = document.querySelectorAll(".save")
+	const editBtn = document.querySelectorAll(".edit")
+	const content = textArea[index].value
+
+	editPost(id, content)
+
+	textArea[index].setAttribute("disabled", "")
+	textArea[index].classList.remove("textarea-on")
+	saveBtn[index].classList.remove("show-btn")
+	editBtn[index].classList.remove("hide")
 }
 
 window.addEventListener("load", async () => await displayPosts())
